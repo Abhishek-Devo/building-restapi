7785055f4a9c081627b2efe75a47450dadb1f698
Abhishek <abhisheksinghnegi862@gmail.com>
2026-03-24 20:36:16 +0530

    first commit

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,31 @@
+import ApiError from "../../common/utils/api-error.js"
+import { generateResetToken } from "../../common/utils/jwt.utils.js";
+import User from "./auth.model.js"
+
+const register = async ({name, email, password, role})=> {
+    
+    const existing = await User.findOne({email})
+    if(existing) throw ApiError.conflict("Email already exisits");
+
+    const {rawToken, hashedToken} = generateResetToken()
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        verificationToken: hashedToken
+    })
+
+    // TODO: send an email to user with token: rawToken
+
+    //js object from mongo object
+    const userObj = user.toObject()
+    delete userObj.password
+    delete userObj.verificationToken
+
+    return userObj
+}
+
+
+export {register}
